5a6b71569d265f2b361046c821eeedf50b3ee85f
Sagar Awale <sagar9510@gmail.com>
2020-03-16 14:24:51 +0000

    Variable Interface

--- SmartHome_Interface/test.py
+++ SmartHome_Interface/test.py
@@ -1,14 +1,14 @@
-# import sys
-# import time
-# import clr
+import sys
+import time
+import clr
 
-# clr.AddReference('EngineIO')
+clr.AddReference('EngineIO')
 
-# from EngineIO import *
+from EngineIO import *
 
 
 
-# livingRoomLight = MemoryMap.Instance.GetFloat(56, MemoryType.Output)
+livingRoomLight = MemoryMap.Instance.GetFloat(56, MemoryType.Output)
 
 
 # print(dir(MemoryMap))
@@ -30,7 +30,20 @@
 # # # When we no longer need the MemoryMap we should call the Dispose method to release all the allocated resources.
 # # MemoryMap.Instance.Dispose()
 
-from globals import *
+# from globals import *
 
-for x in memoryAdd:
-    print(x)
+# for x in memoryAdd:
+#     print(x)
+
+
+for i in range(10):
+	livingRoomLight.Value = i
+	time.sleep(2)
+
+	# When using a memory value before calling the Update method we are using a cached value.
+	print("Light is on? " + str(livingRoomLight.Value))
+
+	# Calling the Update method will write the livingRoomLight.Value to the memory map.
+	MemoryMap.Instance.Update()
+	
+	time.sleep(1)
